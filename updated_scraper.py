3db46713dd52a06fce6b3086c4503e13a32267dd
danielfalqui <100230158+danielfalqui@users.noreply.github.com>
2023-09-14 17:49:07 -0300

    Add files via upload

--- updated_scraper.py
+++ updated_scraper.py
@@ -33,7 +33,47 @@ time.sleep(10)
 scraped_data = []
 
 def scrape():
-    bright_star_table = soup.find("table")
+    bright_star_table = soup.find("table", attrs={"class","wikitable"})
+
+    table_body = bright_star_table.find('tbody')
+
+    table_rows = table_body.find_all('tr')
+
+    
+
+    for row in table_rows:
+        table_cols = row.find_all('td')
+        print(table_cols)
+
+        temp_list = []
+
+        for col_data in table_cols:
+            data = col_data.text.strip()
+            print(data)
+
+            temp_list.append(data)
+
+        scraped_data.append(temp_list)
+
+
+    stars_data = []
+
+    for i in range(0,len(scraped_data)):
+
+        Stars_names = scraped_data[i][1]
+        Distance = scraped_data[i][3]
+        Mass = scraped_data[i][5]
+        Radius = scraped_data[i][6]
+        Lum = scraped_data[i][7]
+
+        required_data = [Stars_names,Distance,Mass,Radius,Lum]
+        stars_data.append[required_data]
+
+    headers = ['Star_name','Distance','Mass','Radius','Luminosity']
+
+    star_df_1 = pd.DataFrame(stars_data, columns=headers)
+
+    star_df_1.to_csv('scraped_data.csv', index=True, index_label="id")
             
 
 
